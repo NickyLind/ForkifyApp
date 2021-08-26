391e1611ea24fb8a7093a195e1198f2427d165ce
Nick Lindau <nicholaithegreat@gmail.com>
2021-08-25 22:17:43 -0700

    Implement basic pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -50,7 +50,8 @@ const controlSearchResults = async function() {
     await model.loadSearchResults(query);
 
     //* 3) Render Results
-    resultsView.render(model.state.search.results)
+    // resultsView.render(model.state.search.results)
+    resultsView.render(model.getSearchResultsPage());
   } catch (error) {
     console.log(error);
   }
--- src/js/model.js
+++ src/js/model.js
@@ -1,11 +1,14 @@
-import { API_URL } from "./config.js";
+import { API_URL, RES_PER_PAGE } from "./config.js";
 import { getJSON } from './helpers.js';
+
 
 export const state = {
   recipe: {},
   search: {
     query: '',
     results: [],
+    page: 1,
+    resultsPerPage: RES_PER_PAGE
   }
 };
 
@@ -48,4 +51,12 @@ export const loadSearchResults = async function(query) {
     console.error(`error 💊🩸🛢 ${error.message}`);
     throw error;
   }
+};
+
+export const getSearchResultsPage = function(page = state.search.page) {
+  state.search.page = page;
+
+  const start = (page - 1) * state.search.resultsPerPage //0;
+  const end = (page) * state.search.resultsPerPage //9;
+  return state.search.results.slice(start, end)
 };
